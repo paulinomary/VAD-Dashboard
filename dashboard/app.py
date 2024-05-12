623d26260307f4b81a9ede173e45dada9daa6f6e
Alexandre Tapadinhas <alexandre22tapadinhas@gmail.com>
2024-05-12 23:17:33 +0100

    Pages working

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,27 +1,19 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-external_stylesheets = ['https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css', 'styles.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+external_stylesheets = ['https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css', 'assets/css/styles.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets, use_pages=True)
 
 app.layout = html.Div([
-    html.Div(className='sidenav', children=[
-        html.Div(className='sidenav-header', children=''),
-        html.A('Home', href='/', className='active'),
     html.Nav(className='sidenav', children=[
         html.A(html.Img(src='./assets/components/Mini Logo branco.png', className='logo'), href='/'),
         html.A('Basic', href='basic'),
         html.A('Advanced', href='advanced'),
         html.A('About Us', href='about-us'),
     ]),
-    html.Div(className='content',
-             #Insert logo in the center of the page
-             children=[
-				 html.Img(src='./assets/components/Logo.png', className='center'),
-				 html.H1('Welcome to our Dashboard'),
-				 html.P('This is a simple dashboard example using Dash and Plotly.')
-			 ])
+    dash.page_container,
 ])
-])
+
+
 if __name__ == '__main__':
     app.run_server(debug=True)
---
+++ dashboard/pages/home.py
@@ -0,0 +1,18 @@
+import dash
+from dash import dcc, html
+import os
+
+# Path to your CSS file (relative or absolute)
+stylesheet_path = os.path.join("assets", "css", "styles.css")
+
+dash.register_page(__name__, path='/')
+
+# Layout with centered logo
+layout = html.Div(
+    children=[
+        # Include your navbar or sidebar component here (if applicable)
+        html.Div(className='content',  # Main content area
+            style={'display': 'flex', 'justify-content': 'center', 'align-items': 'center'},
+            children=[
+                html.Img(src='./assets/components/Logo.png', className='big-logo')
+        ])])
